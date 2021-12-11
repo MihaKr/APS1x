862609adae460d0802bdc6e4eb381c4256564869
miha <miha.kristofelc@gmail.com>
2021-12-12 00:00:33 +0100

    dodal insert

--- src/Naloga2.java
+++ src/Naloga2.java
@@ -5,6 +5,8 @@ public class Naloga2 {
         Scanner scanner = new Scanner(System.in);
         String[] ops = scanner.nextLine().split(" ");
         String[] num = scanner.nextLine().split(" ");
+
+        sorter sort = new sorter(ops, num);
     }
 }
 
@@ -14,14 +16,38 @@ class sorter {
     private String dir;
     private String type;
 
-    public sorter(String[] in) {
-        this.field = new int[in.length];
-        for (int i = 0; i < in.length; i++) {
-            this.field[i] = Integer.parseInt(in[i]);
+    public sorter(String[] ops, String[] num) {
+        this.field = new int[num.length];
+        for (int i = 0; i < num.length; i++) {
+            this.field[i] = Integer.parseInt(num[i]);
         }
+        dir = ops[2];
+        mode = ops[0];
+        type = ops[1];
+
+        insertSort();
     }
 
+    void insertSort() {
+        int ops = 0;
+        for (int i = 0; i < this.field.length; i++) {
+            int key = this.field[i];
+            int j = i - 1;
 
-
-
+            if(this.dir.equals("up")) {
+                while (j >= 0 && this.field[j] > key) {
+                    this.field[j + 1] = this.field[j];
+                    j = j - 1;
+                }
+            }
+            else {
+                while (j >= 0 && this.field[j] < key) {
+                    this.field[j + 1] = this.field[j];
+                    j = j - 1;
+                }
+            }
+            this.field[j + 1] = key;
+        }
+        System.out.println(12);
+    }
 }
